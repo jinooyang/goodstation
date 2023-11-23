a335e6f2eb6eb2139c2a9c6b562cdd943993ed94
LeeHyoungwoo <dlguddn3024@gmail.com>
2023-11-23 19:35:05 +0900

    feat: 공지사항 작성 기능 구현

--- Frontend/src/views/Board/NewsView.vue
+++ Frontend/src/views/Board/NewsView.vue
@@ -1,81 +1,82 @@
-<script setup>
+  <script setup>
 
-import {onMounted, ref} from "vue";
-import axios from 'axios';
-import Pagenation from "@/components/Pagenation.vue";
-import {useMemberStore} from "@/stores/member";
-const memberStore = useMemberStore();
-const {userInfo} = memberStore;
+  import {onMounted, ref} from "vue";
+  import axios from 'axios';
+  import Pagenation from "@/components/Pagenation.vue";
+  import {useMemberStore} from "@/stores/member";
+  const memberStore = useMemberStore();
+  const {userInfo} = memberStore;
 
-const contents = ref([{}]);
+  const contents = ref([{}]);
 
-console.log("userInfo : " + userInfo);
+  console.log("userInfo : " + userInfo);
 
-async function fetchDataFromServer() {
-  try {
-    const response = await axios.get("http://localhost:8080/news");
-    contents.value = response.data;
-    console.log("공지사항을 성공적으로 불러왔습니다.");
-  } catch (error) {
-    console.error("공지사항을 불러오는데 실패했습니다.", error);
+  async function fetchDataFromServer() {
+    try {
+      const response = await axios.get("http://localhost:8080/news");
+      contents.value = response.data;
+      console.log("공지사항을 성공적으로 불러왔습니다.");
+    } catch (error) {
+      console.error("공지사항을 불러오는데 실패했습니다.", error);
+    }
   }
-}
 
-onMounted(async () => {
-  fetchDataFromServer();
-});
-</script>
+  onMounted(async () => {
+    fetchDataFromServer();
+  });
+  </script>
 
-<template>
-  <v-container>
-    <v-row>
-      <v-col :cols="4" offset="4" class="text-center">
-        <div class="title mt-9 mb-9 Jalnan"><h1>공지사항</h1></div>
-      </v-col>
-      <v-col :cols="2" offset="2" class = "d-flex align-center">
+  <template>
+    <v-container>
+      <v-row>
+        <v-col :cols="4" offset="4" class="text-center">
+          <div class="title mt-9 mb-9 Jalnan"><h1>공지사항</h1></div>
+        </v-col>
+        <v-col :cols="2" offset="2" class = "d-flex align-center">
+          <router-link to="/news-write">
+            <v-btn v-if="userInfo!==null && userInfo.role==='ADMIN'" color="red-accent-3 Jalnan">글쓰기</v-btn>
+          </router-link>
+        </v-col>
+      </v-row>
 
-        <v-btn v-if="userInfo!==null && userInfo.role==='ADMIN'" color="red-accent-3 Jalnan">글쓰기</v-btn>
-      </v-col>
-    </v-row>
+      <hr/>
+      <div class="tableborder text-center">
+        <v-table fixed-header>
+          <thead>
+          <tr>
+            <th class="text-center">번호</th>
+            <th class="text-center">제목</th>
+            <th class="text-center">작성자</th>
+            <th class="text-center">작성일</th>
+            <th class = "text-center">조회</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr v-for="content in contents" :key="content.boardId">
+            <td>{{content.newsId}}</td>
+            <td>{{ content.title }}</td>
+            <td>{{ content.memberRole }}</td>
+            <td>{{ content.createTime }}</td>
+            <td><v-btn variant="outlined" color="red-accent-3">글보기</v-btn></td>
+          </tr>
+          <tr >
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          </tbody>
+        </v-table>
 
-    <hr/>
-    <div class="tableborder text-center">
-      <v-table fixed-header>
-        <thead>
-        <tr>
-          <th class="text-center">번호</th>
-          <th class="text-center">제목</th>
-          <th class="text-center">작성자</th>
-          <th class="text-center">작성일</th>
-          <th class = "text-center">조회</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr v-for="content in contents" :key="content.boardId">
-          <td>{{content.newsId}}</td>
-          <td>{{ content.title }}</td>
-          <td>{{ content.memberRole }}</td>
-          <td>{{ content.createTime }}</td>
-          <td><v-btn variant="outlined" color="red-accent-3">글보기</v-btn></td>
-        </tr>
-        <tr >
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-        </tbody>
-      </v-table>
+      </div>
+      <Pagenation/>
+    </v-container>
+  </template>
 
-    </div>
-    <Pagenation/>
-  </v-container>
-</template>
-
-<style scoped>
-th {
-  font-family: Jalnan;
-}
+  <style scoped>
+  th {
+    font-family: Jalnan;
+  }
 
 
-</style>
+  </style>
